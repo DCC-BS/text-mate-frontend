b778244720db2a487cf1bf630b66bcb3975c006c
Tobias Bollinger <tobias.bollinger@bs.ch>
2025-04-23 10:57:24 +0200

    feat: Reorder import statements in string-parser test file for consistency

--- tests/assets/services/string-parser.test.ts
+++ tests/assets/services/string-parser.test.ts
@@ -1,8 +1,8 @@
-import { describe, it, expect } from "vitest";
+import { describe, expect, it } from "vitest";
 import {
     isAlphanumeric,
-    isWhiteSpace,
     isSentenceEnd,
+    isWhiteSpace,
     splitToSentences,
 } from "../../../assets/services/string-parser";
 
