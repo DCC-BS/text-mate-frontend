79890a0e1c6268717ba4ea4a8dbc593a84d049c4
Tobias Bollinger <tobias.bollinger@bs.ch>
2025-07-08 17:51:47 +0200

    feat(api): implement fetcher function for quick-action POST request

--- server/api/quick-action.post.ts
+++ server/api/quick-action.post.ts
@@ -1,4 +1,11 @@
 export default defineBackendHandler({
     url: "/quick-action",
     method: "POST",
+    fetcher: async (url, method, body, headers) => {
+        return await fetch(url, {
+            method,
+            body: JSON.stringify(body),
+            headers: headers,
+        });
+    },
 });
