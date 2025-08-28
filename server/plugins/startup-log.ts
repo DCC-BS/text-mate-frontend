829d9f925eecb7b029812d309505ae444ed910b6
Yanick Schraner <yanick.schraner@gmail.com>
2025-08-28 10:28:09 +0200

    do not log secrets

--- server/plugins/startup-log.ts
+++ server/plugins/startup-log.ts
@@ -1,3 +1,14 @@
+const filter_secrets = (str: string) => {
+    const secrets = ["githubToken"];
+    return secrets.reduce((acc, secret) => {
+        return acc.replace(
+            new RegExp(`"${secret}":"([^"]+)"`, "g"),
+            `"${secret}":"********"`,
+        );
+    }, str);
+};
+
+
 export default defineNitroPlugin(() => {
     if (import.meta.dev) {
         return;
@@ -7,5 +18,5 @@ export default defineNitroPlugin(() => {
 
     const config = useRuntimeConfig();
 
-    console.log("Runtime config", config);
+    console.log("Runtime config", filter_secrets(JSON.stringify(config)));
 });
