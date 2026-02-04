1f8ac3abe699d1c735922785b21e315dcd4ca33f
Tobias Bollinger <tobias.bollinger@gmail.com>
2026-02-04 21:36:04 +0100

    wip

--- server/plugins/env-validation.ts
+++
@@ -1,7 +0,0 @@
-import { validateRuntimeEnv } from "#shared/types/env";
-
-export default defineNitroPlugin(() => {
-    const runtimeConfig = useRuntimeConfig();
-
-    validateRuntimeEnv(runtimeConfig);
-});
