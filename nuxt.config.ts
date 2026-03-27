8834deda2c06be6c74382956ca5c1178d12c1b48
Yanick Schraner <yanick.schraner@bs.ch>
2026-03-27 12:22:19 +0100

    Fix nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -93,15 +93,10 @@ export default defineNuxtConfig({
             rollupOptions: {
                 output: {
                     // Use manual chunks to improve chunking
-                    manualChunks: {
-                        "vue-vendor": ["vue", "vue-router"],
-                        "tiptap-vendor": [
-                            "@tiptap/vue-3",
-                            "@tiptap/starter-kit",
-                            "@tiptap/extension-bubble-menu",
-                            "@tiptap/extension-character-count",
-                        ],
-                        "pdf-vendor": ["vue-pdf-embed"],
+                    manualChunks: (id) => {
+                        if (["vue", "vue-router"].some((pkg) => id.includes(`/node_modules/${pkg}/`))) return "vue-vendor";
+                        if (["@tiptap/vue-3", "@tiptap/starter-kit", "@tiptap/extension-bubble-menu", "@tiptap/extension-character-count"].some((pkg) => id.includes(`/node_modules/${pkg}/`))) return "tiptap-vendor";
+                        if (id.includes("/node_modules/vue-pdf-embed/")) return "pdf-vendor";
                     },
                 },
             },
