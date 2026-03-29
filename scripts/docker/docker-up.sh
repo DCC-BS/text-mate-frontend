d40bb5219321f97e3581659432aca404f06343f5
Tobias Bollinger <tobias.bollinger@gmail.com>
2026-03-29 12:16:38 +0200

    working on env for docker

--- scripts/docker/docker-up.sh
+++ scripts/docker/docker-up.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -e
 
-pwd
-
-bunx varlock run -p ./docker -- docker compose -f docker-compose.dev.yml up --build -d
+cd docker
+bunx varlock run -- docker compose -f docker-compose.dev.yml up --build -d
