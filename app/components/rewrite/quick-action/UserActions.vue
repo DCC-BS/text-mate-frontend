bcd7ce2ba8f452cfafe6b74f7eef7a4cf281172d
Tobias Bollinger <tobias.bollinger@bs.ch>
2026-07-27 14:23:10 +0200

    fixed biome linting

--- app/components/rewrite/quick-action/UserActions.vue
+++ app/components/rewrite/quick-action/UserActions.vue
@@ -1,8 +1,8 @@
 <script lang="ts" setup>
-import { tooltip } from "#build/ui";
-import { UButton, UTooltip } from "#components";
 import { apiFetch, isApiError } from "@dcc-bs/communication.bs.js";
 import type { DropdownMenuItem } from "@nuxt/ui";
+import { tooltip } from "#build/ui";
+import { UButton, UTooltip } from "#components";
 import {
     type TextAction,
     TextActionGetOutputSchema,
@@ -39,28 +39,36 @@ onMounted(async () => {
 });
 
 const items = computed<DropdownMenuItem[]>(() =>
-    userActions.value.map((x) => ({
-        label: x.name,
-        value: x.id,
-        tooltip: x.tooltip,
-        onSelect: () => emit("apply-action", x.id),
-    } satisfies DropdownMenuItem)),
+    userActions.value.map(
+        (x) =>
+            ({
+                label: x.name,
+                value: x.id,
+                tooltip: x.tooltip,
+                onSelect: () => emit("apply-action", x.id),
+            }) satisfies DropdownMenuItem,
+    ),
 );
 </script>
 
 <template>
     <UDropdownMenu :items="items" v-if="items.length > 0">
-            <UButton
-                variant="link"
-                color="neutral"
-                size="sm"
-                :disabled="!props.actionsAreAvailable"
-            >
-                {{ t("editor.userActions") }}
-            </UButton>
+        <UButton
+            variant="link"
+            color="neutral"
+            size="sm"
+            :disabled="!props.actionsAreAvailable"
+        >
+            {{ t("editor.userActions") }}
+        </UButton>
         <template #item="{ item }">
             <UTooltip :text="item.tooltip">
-                <UButton class="p-0" variant="link" color="neutral"  @click="item.onSelect">
+                <UButton
+                    class="p-0"
+                    variant="link"
+                    color="neutral"
+                    @click="item.onSelect"
+                >
                     {{ item.label }}
                 </UButton>
             </UTooltip>
